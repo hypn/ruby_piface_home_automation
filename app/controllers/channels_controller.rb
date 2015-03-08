ad373de942a55354378da1a6cdf3959bd042f424
Ross <ross.simpson@gmail.com>
2015-03-08 19:52:11 +0200

    fixed some RuboCop warnings

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,10 +1,10 @@
 class ChannelsController < ApplicationController
-  before_action :get_channel
+  before_action :channel
 
   begin
     require 'piface'
   rescue Exception
-    puts "Error: piface not defined!"
+    puts 'Error: piface not defined!'
   end
 
   def on
@@ -19,46 +19,41 @@ class ChannelsController < ApplicationController
     render text: read_channel(@channel)
   end
 
-private
+  private
 
-  def get_channel
+  def channel
     @channel = (params[:id].to_i) - 1
   end
 
   def input_state_to_word(state)
-    return (state == 1) ? "on" : "off"
+    return (state == 1) ? 'on' : 'off'
   end
 
   def set_channel(channel, state) # turn a channel on or off
     if defined? Piface
       Piface.write channel, state
     else
-      puts "!"
-      puts "! Piface not present, setting channel #{channel} to #{state}"
-      puts "!"
+      puts "!\n! Piface not present, setting channel #{channel} to #{state}\n!"
     end
 
     sleep 1
 
     if read_channel(channel) != input_state_to_word(state)
       Piface.write(channel, 0) if defined? Piface
-      return "error"
+      return 'error'
     else
-      return (state == 1) ? "on" : "off"
+      return (state == 1) ? 'on' : 'off'
     end
   end
 
   def read_channel(channel) # get "on/off" for channel number
     if defined? Piface
-      state = (Piface.read(channel) == 1) ? "on" : "off"
+      state = (Piface.read(channel) == 1) ? 'on' : 'off'
     else
-      state = (rand(2) == 1) ? "on" : "off"
-      puts "!"
-      puts "! Piface not present, reading channel #{channel} and returning '#{state}'"
-      puts "!"
+      state = (rand(2) == 1) ? 'on' : 'off'
+      puts "!\n! Piface not present, returning '#{state}' for channel #{channel}\n!"
     end
 
     return state
   end
-
 end
